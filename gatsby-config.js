38dc96d97a28d75db5958930f9f712f935373437
Christopher Lamm <chris@theantichris.com>
2021-05-07 09:36:28 -0400

    build: added gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,16 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: "theantichris.com",
+    description: "Christopher Lamm's personal site."
+  },
+  plugins: [
+    "gatsby-transformer-remark",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: `${__dirname}/src/pages`
+      }
+    }
+  ],
 }
