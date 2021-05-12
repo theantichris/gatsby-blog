0cceed19d6663985aafcf7fe94170b354e8d7f18
Christopher Lamm <chris@theantichris.com>
2021-05-12 09:49:31 -0400

    feature: start of blog post template and link to posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
 import Header from "../components/Header"
 
 const Layout = ({data}) => {
@@ -22,7 +22,9 @@ const Layout = ({data}) => {
             <div
             key={frontmatter.path}
             style={{marginBottom: "1rem"}}>
-              {frontmatter.title}
+              <Link to={frontmatter.path}>
+                {frontmatter.title}
+              </Link>
             </div>
           )
         })}
